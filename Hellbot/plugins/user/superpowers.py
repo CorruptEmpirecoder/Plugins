5c07ddc38a87ba5dc367c2bcfdb2ec3a9704aaf4
CorruptEmpirecoder <156877548+CorruptEmpirecoder@users.noreply.github.com>
2024-04-12 13:40:38 +0530

    Update superpowers.py

--- Hellbot/plugins/user/superpowers.py
+++ Hellbot/plugins/user/superpowers.py
@@ -546,37 +546,37 @@ HelpMenu("superpowers").add(
     "gpromote",
     "<reply/username/id> <reason (optional)>",
     "Promote a user in all the chats where you have add admin right.",
-    "gpromote @ForGo10God Why not?",
+    "gpromote @BloodAvi Why not?",
 ).add(
     "gdemote",
     "<reply/username/id> <reason (optional)>",
     "Demotes a user in all the chats where you are on top level from the user.",
-    "gdemote @ForGo10God Why?",
+    "gdemote @BloodAvi Why?",
 ).add(
     "gban",
     "<reply/username/id> <reason (optional)>",
     "Ban a user in all the chats where you have ban rights.",
-    "gban @ForGo10God :)",
+    "gban @BloodAvi :)",
 ).add(
     "ungban",
     "<reply/username/id>",
     "Unban a user in all the chats where you have ban rights.",
-    "ungban @ForGo10God",
+    "ungban @BloodAvi",
 ).add(
     "gkick",
     "<reply/username/id> <reason (optional)>",
     "Kick a user in all the chats where you have ban rights.",
-    "gkick @ForGo10God :)",
+    "gkick @BloodAvi :)",
 ).add(
     "gmute",
     "<reply/username/id> <reason (optional)>",
     "Mute a user in all the chats where you have mute rights.",
-    "gmute @ForGo10God :)",
+    "gmute @BloodAvi :)",
 ).add(
     "ungmute",
     "<reply/username/id>",
     "Unmute a user in all the chats where you have mute rights.",
-    "ungmute @ForGo10God",
+    "ungmute @BloodAvi",
 ).add(
     "gbanlist", None, "List all the gbanned users.", "gbanlist"
 ).add(
